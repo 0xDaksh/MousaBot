11a02a642b05c03bc6cfd36ea93f16725c7549c1
Daksh Miglani <hello@dak.sh>
2017-11-12 17:15:22 +0530

    skinprices improvised and removed stattrak

--- bot/commands.js
+++ bot/commands.js
@@ -28,8 +28,8 @@ export default (bot) => {
 						if(typeof body.error !== 'undefined' && body.error !== '' && body.error.toLowerCase() == 'pair not found') {
 							msg.reply(' Currency Not Found!')
 						}
-						if(body.ticker && body.ticker.price && body.ticker.volume) {
-							msg.reply(` The Price of ${body.ticker.base} in ${body.ticker.target} is ${parseFloat(body.ticker.price).toFixed(2)} and the Volume is ${parseFloat(body.ticker.volume).toFixed(2)}`)
+						if(body.ticker && body.ticker.price) {
+							msg.reply(` The Price of ${body.ticker.base} in ${body.ticker.target} is ${parseFloat(body.ticker.price).toFixed(2)} and the Volume is ${body.ticker.volume !== '' ? parseFloat(body.ticker.volume).toFixed(2) : 'Nill'}`)
 						}
 					} else {
 						msg.reply(' Cannot Fetch The Prices atm, Please Check if the Currency is available or not.')
@@ -45,12 +45,19 @@ export default (bot) => {
 				msg.reply(' Can you Please Provide the Name of the Skin?')
 			} else {
 				let name = msg.content.split('=skinprice ')[1].toLowerCase()
-				console.log(name)
+				let flag = 0
 				for(let i = 0; i < Object.keys(skinPrices.prices).length; i++) {
-					if(Object.keys(skinPrices.prices)[i].toLowerCase().indexOf(name) !== -1) {
+					if(
+						Object.keys(skinPrices.prices)[i].toLowerCase().indexOf(name) !== -1 &&
+						Object.keys(skinPrices.prices)[i].toLowerCase().indexOf('stattrak') === -1
+					) {
 						msg.reply('The price of: ' + Object.keys(skinPrices.prices)[i] + ' is ' + skinPrices.prices[Object.keys(skinPrices.prices)[i]])
+						flag = 1
 						break
-					}
+					} 
+				}
+				if(flag == 0) {
+					msg.reply(' The Skin was not found!')
 				}
 			}
 		}
